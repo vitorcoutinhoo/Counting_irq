67b35f397b09f8a6f6dfae8bedda525a851220d4
Vitor <vitorcooutinho@gmail.com>
2025-01-28 21:06:55 -0300

    feat: implement red led blink function

--- tarefa1_27_01.c
+++ tarefa1_27_01.c
@@ -1,14 +1,34 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definindo o pino vermelho do led RGB
+#define RED_LED 13
 
 
-int main()
-{
+// Protótipos das funções
+void init_gpio(uint gpio, uint dir);
+void red_blink(uint gpio, uint freq_ms);
+
+
+int main() {
     stdio_init_all();
+    init_gpio(RED_LED, GPIO_OUT);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        red_blink(RED_LED, 200);
     }
+}
+
+// Inicia e seta se o pino é entrada ou saida, coloca o pino em nivel baixo
+void init_gpio(uint gpio, uint dir){
+    gpio_init(gpio);
+    gpio_set_dir(gpio, dir);
+    gpio_put(gpio, 0);
+}
+
+// Função que pisca o LED vermelho 5 vezes por segundo
+void red_blink(uint gpio, uint freq_ms){
+    gpio_put(gpio, 1);
+    sleep_ms(freq_ms);
+    gpio_put(gpio, 0);
+    sleep_ms(freq_ms);
 }
