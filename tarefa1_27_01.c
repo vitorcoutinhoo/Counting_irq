56eaa134f84579aa937538497330d437684825ef
Vitor <vitorcooutinho@gmail.com>
2025-01-29 18:39:24 -0300

    feat: implement functions for led matrix

--- tarefa1_27_01.c
+++ tarefa1_27_01.c
@@ -9,12 +9,14 @@
 
 // Definindo a matriz de leds
 #define NLED 25
-static const uint32_t LEDS[NLED];
+static uint32_t LEDS[NLED];
 
 
 // Protótipos das funções
 void init_gpio(uint gpio, uint dir);
 void red_blink(uint gpio, uint freq_ms);
+static inline uint32_t color(uint8_t r, uint8_t g, uint8_t b);
+static inline void put_pixel(uint32_t pixel_grb);
 
 
 int main() {
@@ -27,7 +29,12 @@ int main() {
     uint offset = pio_add_program(pio, &ws2812_program);
     ws2812_program_init(pio, sm, offset, LED_MATRIX, 800000, false);
 
+    uint32_t white = color(96, 96, 96);
+
     while (true) {
+        LEDS[12] = white;
+        for (int i = 0; i < NLED; i++)
+            put_pixel(LEDS[i]);
         red_blink(RED_LED, 100);
     }
 }
@@ -46,3 +53,15 @@ void red_blink(uint gpio, uint freq_ms){
     gpio_put(gpio, 0);
     sleep_ms(freq_ms);
 }
+
+// Função para colocar a cor do LED  na matriz
+static inline void put_pixel(uint32_t pixel_grb) {
+    pio_sm_put_blocking(pio0, 0, pixel_grb);
+}
+
+// Função para representar a cor em formato RGB
+static inline uint32_t color(uint8_t r, uint8_t g, uint8_t b) {
+    return ((uint32_t)(g) << 24) | ((uint32_t)(r) << 16) | ((uint32_t)(b) << 8);
+}
+
+
