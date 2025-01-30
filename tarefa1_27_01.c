fb2a23b1faa5aa4d526a56850cce5f1b1c4f722e
Vitor <vitorcooutinho@gmail.com>
2025-01-29 23:51:41 -0300

    feat: implement display number function

--- tarefa1_27_01.c
+++ tarefa1_27_01.c
@@ -1,6 +1,8 @@
 // Bibliotecas necessárias
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "hardware/dma.h" 
 #include "ws2812.pio.h"
 
 // Definindo os pinos utilizados
@@ -11,12 +13,27 @@
 #define NLED 25
 static uint32_t LEDS[NLED];
 
+// Números a serem mostrados
+static uint32_t numbers[10][NLED] = {
+    {0, 1, 1, 1, 0,  0, 1, 0, 1, 0,  0, 1, 0, 1, 0,  0, 1, 0, 1, 0,  0, 1, 1, 1, 0}, // 0
+    {0, 1, 1, 1, 0,  0, 0, 1, 0, 0,  0, 0, 1, 0, 0,  0, 1, 1, 0, 0,  0, 0, 1, 0, 0}, // 1
+    {0, 1, 1, 1, 0,  0, 1, 0, 0, 0,  0, 1, 1, 0, 0,  0, 1, 0, 1, 0,  0, 1, 1, 1, 0}, // 2
+    {0, 1, 1, 1, 0,  0, 0, 0, 1, 0,  0, 1, 1, 1, 0,  0, 0, 0, 1, 0,  0, 1, 1, 1, 0}, // 3
+    {0, 1, 0, 0, 0,  0, 0, 0, 1, 0,  0, 1, 1, 1, 0,  0, 1, 0, 1, 0,  0, 1, 0, 1, 0}, // 4
+    {0, 1, 1, 1, 0,  0, 0, 0, 1, 0,  0, 0, 1, 1, 0,  0, 1, 0, 0, 0,  0, 1, 1, 1, 0}, // 5
+    {0, 1, 1, 1, 0,  0, 1, 0, 1, 0,  0, 1, 1, 1, 0,  0, 1, 0, 0, 0,  0, 1, 1, 0, 0}, // 6
+    {0, 0, 1, 0, 0,  0, 0, 1, 0, 0,  0, 0, 1, 0, 0,  0, 0, 0, 1, 0,  0, 1, 1, 1, 0}, // 7
+    {0, 1, 1, 1, 0,  0, 1, 0, 1, 0,  0, 1, 1, 1, 0,  0, 1, 0, 1, 0,  0, 1, 1, 1, 0}, // 8
+    {0, 0, 1, 1, 0,  0, 0, 0, 1, 0,  0, 1, 1, 1, 0,  0, 1, 0, 1, 0,  0, 1, 1, 1, 0}  // 9
+};
+
 
 // Protótipos das funções
 void init_gpio(uint gpio, uint dir);
 void red_blink(uint gpio, uint freq_ms);
 static inline uint32_t color(uint8_t r, uint8_t g, uint8_t b);
 static inline void put_pixel(uint32_t pixel_grb);
+void display_number(uint8_t num);
 
 
 int main() {
@@ -29,12 +46,8 @@ int main() {
     uint offset = pio_add_program(pio, &ws2812_program);
     ws2812_program_init(pio, sm, offset, LED_MATRIX, 800000, false);
 
-    uint32_t white = color(96, 96, 96);
-
     while (true) {
-        LEDS[12] = white;
-        for (int i = 0; i < NLED; i++)
-            put_pixel(LEDS[i]);
+        display_number(9);
         red_blink(RED_LED, 100);
     }
 }
@@ -52,6 +65,17 @@ void red_blink(uint gpio, uint freq_ms){
     sleep_ms(freq_ms);
     gpio_put(gpio, 0);
     sleep_ms(freq_ms);
+}
+
+
+// Função para mostrar o numero na matriz
+void display_number(uint8_t num) {
+    uint32_t white = color(10, 10, 0);
+
+    for (uint j = 0; j < NLED; j++) {
+        if (numbers[num][j]) put_pixel(white);
+        else put_pixel(0);
+    }
 }
 
 // Função para colocar a cor do LED  na matriz
